82a65cdeca791872e77cb23877829c7de54a695c
ShoaibShokat03 <shoaibshokat03@gmail.com>
2025-08-07 22:31:13 +0500

    updated path detection in file and server to send all request on index.php

--- src/core/Router.php
+++ src/core/Router.php
@@ -18,8 +18,11 @@ class Router
 
     public function dispatch(string $method, string $uri)
     {
+        $basePath = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])), '/');
+        $basePath = str_replace("/public", "", $basePath);
+
         // Remove base path
-        $targetRoute = str_replace($_ENV['BASE_URL'].'/api/', '/', $uri);
+        $targetRoute = str_replace($basePath . '/api/', '/', $uri);
 
         foreach ($this->routes as $route) {
             if ($route['method'] !== strtoupper($method)) continue;
